cbb2d136f53a536467ad96dc7c885c373a0ca7e4
Jay Khakim <mgmediajay@gmail.com>
2023-12-23 23:01:19 +0900

    nav section

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,6 +9,7 @@ const Nav = () => {
   const isUserLoggenIn = true
 
   const [providers, setPorviders] = useState(null)
+  const [toglleDropdown, setToggleDropdown] = useState(false)
 
   useEffect(() => {
     const setProviders = async()=>{
@@ -47,8 +48,47 @@ const Nav = () => {
               </Link>
             </div>
           ):(
-            <></>
+            <>
+              {providers && Object.values(providers).map((provider) =>(
+                <button type="button" key={provider.name} onClick={()=> signIn(provider.id)} className="black_btn">
+
+                </button>
+              ))}
+            </>
           )}
+        </div>
+
+        {/* Mobile Navigation  */}
+
+        <div className="sm:hidden flex relative">
+            {isUserLoggenIn?(
+              <div className="flex">
+                <Image src="/assets/images/logo.svg" width={37} height={37} className="rounded-full" alt="profile" onClick={()=> setToggleDropdown((prev) =>!prev)}/>
+
+                {toglleDropdown && (
+                  <div className="dropdown">
+                    <Link href="/profile" className="dropdown_link" onClick={()=> setToggleDropdown(false)}>
+                      My Profile
+                    </Link>
+                    <Link href="/create-prompt" className="dropdown_link" onClick={()=> setToggleDropdown(false)}>
+                      Create Prompt
+                    </Link>
+
+                    <button type="button" onClick={()=> {setToggleDropdown(false); signOut}} className="mt-5 w-full black_btn">
+                      Sign Out
+                    </button>
+                  </div>
+                )}
+              </div>
+            ):(
+              <>
+                {providers && Object.values(providers).map((provider) =>(
+                  <button type="button" key={provider.name} onClick={()=> signIn(provider.id)} className="black_btn">
+
+                  </button>
+                ))}
+              </>
+            )}
         </div>
     </nav>
   )
