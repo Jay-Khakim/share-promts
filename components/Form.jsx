43bfc3ea23bd9d2b4bc2f580754d7b61d6d0063c
Jay Khakim <mgmediajay@gmail.com>
2024-02-09 23:20:17 +0900

    ...

--- components/Form.jsx
+++ components/Form.jsx
@@ -42,6 +42,9 @@ const Form = ({
             />
           </label>
 
+
+
+
           <div className='flex-end mx-3 mb-5 gap-4'>
             <Link href="/" className='text-gray-500 text-sm'>
               Cancel
