4a6eba3968460765b75b7965ba5195037149d9af
Jay Khakim <mgmediajay@gmail.com>
2024-03-17 09:31:41 +0900

    typo-fix

--- app/profile/page.js
+++ app/profile/page.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
-import Profile from "@components/profile";
+import Profile from "@components/Profile";
 
 const MyProfile = () => {
 	const { data: session } = useSession();
@@ -25,25 +25,25 @@ const MyProfile = () => {
 		router.push(`/update-prompt?id=${post._id}`);
 	};
 
-	const handleDelete = async (post) => {
+	const handleDelete = async post => {
 		const hasConfirmed = confirm(
-		  "Are you sure you want to delete this prompt?"
+			"Are you sure you want to delete this prompt?"
 		);
-	
+
 		if (hasConfirmed) {
-		  try {
-			await fetch(`/api/prompt/${post._id.toString()}`, {
-			  method: "DELETE",
-			});
-	
-			const filteredPosts = posts.filter((item) => item._id !== post._id);
-	
-			setPosts(filteredPosts);
-		  } catch (error) {
-			console.log(error);
-		  }
+			try {
+				await fetch(`/api/prompt/${post._id.toString()}`, {
+					method: "DELETE",
+				});
+
+				const filteredPosts = posts.filter(item => item._id !== post._id);
+
+				setPosts(filteredPosts);
+			} catch (error) {
+				console.log(error);
+			}
 		}
-	  };
+	};
 	return (
 		<Profile
 			name="My"
